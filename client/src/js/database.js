cbbfef4ea5c988dc1b6bc38b3f0278e85267029c
ChristianAlteri <alteri.christian@gmail.com>
2023-07-28 11:20:16 +0800

    bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,13 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const postDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   console.log('Content: ', content);
-  const result = await store.put({ content: content, id: id });
+  const result = await store.put({ content: content });
   console.log('🚀 - data saved to the database', result);
+};
+
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const result = await store.getAll();
+  console.log('result.value', result);
+  return result;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
